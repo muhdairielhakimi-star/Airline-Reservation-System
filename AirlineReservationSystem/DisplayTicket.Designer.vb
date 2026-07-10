9962f2651f4c8b4ee25f75d96820879b9c228e0a
afrizal06 <2024220448@student.uitm.edu.my>
2026-07-10 23:56:00 +0800

    letak display ticket

---
+++ AirlineReservationSystem/DisplayTicket.Designer.vb
@@ -0,0 +1,322 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTicket
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlTicket = New System.Windows.Forms.Panel()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblAirline = New System.Windows.Forms.Label()
+        Me.lblFrom = New System.Windows.Forms.Label()
+        Me.lblTo = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblProgram = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblFlight = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblGate = New System.Windows.Forms.Label()
+        Me.lblSeat = New System.Windows.Forms.Label()
+        Me.pnlTicket.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlTicket
+        '
+        Me.pnlTicket.Controls.Add(Me.PictureBox1)
+        Me.pnlTicket.Controls.Add(Me.lblTo)
+        Me.pnlTicket.Controls.Add(Me.lblFrom)
+        Me.pnlTicket.Controls.Add(Me.lblAirline)
+        Me.pnlTicket.Location = New System.Drawing.Point(87, 51)
+        Me.pnlTicket.Name = "pnlTicket"
+        Me.pnlTicket.Size = New System.Drawing.Size(626, 130)
+        Me.pnlTicket.TabIndex = 0
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(361, 9)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(83, 24)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "TICKET"
+        '
+        'lblAirline
+        '
+        Me.lblAirline.AutoSize = True
+        Me.lblAirline.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAirline.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.lblAirline.Location = New System.Drawing.Point(3, 0)
+        Me.lblAirline.Name = "lblAirline"
+        Me.lblAirline.Size = New System.Drawing.Size(186, 24)
+        Me.lblAirline.TabIndex = 2
+        Me.lblAirline.Text = "SAUJANA FLIGHT"
+        '
+        'lblFrom
+        '
+        Me.lblFrom.AutoSize = True
+        Me.lblFrom.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFrom.Location = New System.Drawing.Point(28, 90)
+        Me.lblFrom.Name = "lblFrom"
+        Me.lblFrom.Size = New System.Drawing.Size(70, 24)
+        Me.lblFrom.TabIndex = 2
+        Me.lblFrom.Text = "FROM"
+        '
+        'lblTo
+        '
+        Me.lblTo.AutoSize = True
+        Me.lblTo.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTo.Location = New System.Drawing.Point(251, 90)
+        Me.lblTo.Name = "lblTo"
+        Me.lblTo.Size = New System.Drawing.Size(37, 24)
+        Me.lblTo.TabIndex = 3
+        Me.lblTo.Text = "TO"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.DisplayTicket.My.Resources.Resources.images
+        Me.PictureBox1.Location = New System.Drawing.Point(122, 77)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.lblSeat)
+        Me.Panel1.Controls.Add(Me.lblGate)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.lblFlight)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.lblTime)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.lblDate)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.lblProgram)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.lblName)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(87, 197)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(626, 180)
+        Me.Panel1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DimGray
+        Me.Label1.Location = New System.Drawing.Point(28, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 21)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Name:"
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(123, 15)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(60, 21)
+        Me.lblName.TabIndex = 6
+        Me.lblName.Text = "NAME"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DimGray
+        Me.Label2.Location = New System.Drawing.Point(28, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 21)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Program:"
+        '
+        'lblProgram
+        '
+        Me.lblProgram.AutoSize = True
+        Me.lblProgram.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblProgram.Location = New System.Drawing.Point(123, 50)
+        Me.lblProgram.Name = "lblProgram"
+        Me.lblProgram.Size = New System.Drawing.Size(98, 21)
+        Me.lblProgram.TabIndex = 8
+        Me.lblProgram.Text = "PROGRAM"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.DimGray
+        Me.Label4.Location = New System.Drawing.Point(28, 87)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 21)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Date:"
+        '
+        'lblDate
+        '
+        Me.lblDate.AutoSize = True
+        Me.lblDate.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDate.Location = New System.Drawing.Point(123, 87)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(55, 21)
+        Me.lblDate.TabIndex = 10
+        Me.lblDate.Text = "DATE"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.DimGray
+        Me.Label3.Location = New System.Drawing.Point(28, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 21)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Time:"
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTime.Location = New System.Drawing.Point(123, 124)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(53, 21)
+        Me.lblTime.TabIndex = 12
+        Me.lblTime.Text = "TIME"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.DimGray
+        Me.Label5.Location = New System.Drawing.Point(360, 25)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 21)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Flight:"
+        '
+        'lblFlight
+        '
+        Me.lblFlight.AutoSize = True
+        Me.lblFlight.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFlight.Location = New System.Drawing.Point(431, 25)
+        Me.lblFlight.Name = "lblFlight"
+        Me.lblFlight.Size = New System.Drawing.Size(129, 21)
+        Me.lblFlight.TabIndex = 14
+        Me.lblFlight.Text = "FLIGHT NAME"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.DimGray
+        Me.Label6.Location = New System.Drawing.Point(360, 80)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(56, 21)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Gate:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.DimGray
+        Me.Label7.Location = New System.Drawing.Point(468, 80)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(54, 21)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Seat:"
+        '
+        'lblGate
+        '
+        Me.lblGate.AutoSize = True
+        Me.lblGate.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGate.Location = New System.Drawing.Point(363, 112)
+        Me.lblGate.Name = "lblGate"
+        Me.lblGate.Size = New System.Drawing.Size(55, 21)
+        Me.lblGate.TabIndex = 17
+        Me.lblGate.Text = "GATE"
+        '
+        'lblSeat
+        '
+        Me.lblSeat.AutoSize = True
+        Me.lblSeat.Font = New System.Drawing.Font("Tahoma", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSeat.Location = New System.Drawing.Point(469, 112)
+        Me.lblSeat.Name = "lblSeat"
+        Me.lblSeat.Size = New System.Drawing.Size(53, 21)
+        Me.lblSeat.TabIndex = 18
+        Me.lblSeat.Text = "SEAT"
+        '
+        'frmTicket
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.pnlTicket)
+        Me.Name = "frmTicket"
+        Me.Text = "Ticket"
+        Me.pnlTicket.ResumeLayout(False)
+        Me.pnlTicket.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pnlTicket As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblTo As Label
+    Friend WithEvents lblFrom As Label
+    Friend WithEvents lblAirline As Label
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblTime As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblDate As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblProgram As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblName As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblFlight As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblSeat As Label
+    Friend WithEvents lblGate As Label
+End Class
---
+++ AirlineReservationSystem/DisplayTicket.vb
@@ -0,0 +1,34 @@
+﻿Public Class frmTicket
+
+    'Declaration
+    Public Departure As String
+    Public Destination As String
+
+    Public PassengerName As String
+    Public ProgramName As String
+
+    Public FlightDate As String
+    Public FlightTime As String
+
+    Public FlightName As String
+    Public Gate As String
+    Public Seat As String
+
+    Private Sub frmTicket_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        lblFrom.Text = Departure
+        lblTo.Text = Destination
+
+        lblName.Text = PassengerName
+        lblProgram.Text = ProgramName
+
+        lblDate.Text = FlightDate
+        lblTime.Text = FlightTime
+
+        lblFlight.Text = FlightName
+        lblGate.Text = Gate
+        lblSeat.Text = Seat
+
+    End Sub
+
+End Class

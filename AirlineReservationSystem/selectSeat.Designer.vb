72927e9daa20ddb64514ffc5760ae0a85b55ee76
afrizal06 <2024220448@student.uitm.edu.my>
2026-07-10 19:17:10 +0800

    add arman punya part

---
+++ AirlineReservationSystem/selectSeat.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class selectSeat
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblPriceTotal = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnContinue = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmbCabinClass = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(400, 28)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(126, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Seat Select Option"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblPriceTotal)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.btnContinue)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.cmbCabinClass)
+        Me.GroupBox1.Location = New System.Drawing.Point(30, 110)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(331, 477)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Choose Your Seat"
+        '
+        'lblPriceTotal
+        '
+        Me.lblPriceTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblPriceTotal.Location = New System.Drawing.Point(6, 189)
+        Me.lblPriceTotal.Name = "lblPriceTotal"
+        Me.lblPriceTotal.Size = New System.Drawing.Size(293, 42)
+        Me.lblPriceTotal.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 158)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(113, 17)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Seat Price Total:"
+        '
+        'btnContinue
+        '
+        Me.btnContinue.Location = New System.Drawing.Point(77, 421)
+        Me.btnContinue.Name = "btnContinue"
+        Me.btnContinue.Size = New System.Drawing.Size(143, 30)
+        Me.btnContinue.TabIndex = 5
+        Me.btnContinue.Text = "Continue"
+        Me.btnContinue.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Cabin Class:"
+        '
+        'cmbCabinClass
+        '
+        Me.cmbCabinClass.FormattingEnabled = True
+        Me.cmbCabinClass.Location = New System.Drawing.Point(20, 65)
+        Me.cmbCabinClass.Name = "cmbCabinClass"
+        Me.cmbCabinClass.Size = New System.Drawing.Size(274, 24)
+        Me.cmbCabinClass.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(402, 80)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(124, 17)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Seat Not Available"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(402, 129)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(95, 17)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Seat Avaiable"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(402, 180)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(96, 17)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "Seat Selected"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.PictureBox1.Location = New System.Drawing.Point(553, 80)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(38, 30)
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.PictureBox2.Location = New System.Drawing.Point(553, 129)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(38, 30)
+        Me.PictureBox2.TabIndex = 10
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Lime
+        Me.PictureBox3.Location = New System.Drawing.Point(553, 180)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(38, 30)
+        Me.PictureBox3.TabIndex = 11
+        Me.PictureBox3.TabStop = False
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 3
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(626, 80)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 10
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(169, 608)
+        Me.TableLayoutPanel1.TabIndex = 12
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 3
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(825, 80)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 10
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(169, 608)
+        Me.TableLayoutPanel2.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1006, 721)
+        Me.Controls.Add(Me.TableLayoutPanel2)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "Form1"
+        Me.Text = "Seat Selection"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lblPriceTotal As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnContinue As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cmbCabinClass As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+End Class
---
+++ AirlineReservationSystem/selectSeat.vb
@@ -0,0 +1,206 @@
+﻿Public Class selectSeat
+
+    ' Structure to hold seat properties in our array
+    Private Structure SeatData
+        Dim SeatNumber As String
+        Dim CabinClass As String
+        Dim IsBooked As Boolean
+    End Structure
+
+    ' The Master Array holding all 60 plane seats
+    Private seatArray(59) As SeatData
+
+    ' Global tracking variables for selection
+    Private selectedButtons As New List(Of Button)
+    Private totalPrice As Decimal = 0
+
+    ' Pricing Dictionary mapping category to price
+    Private seatPriceMap As New Dictionary(Of String, Decimal) From {
+        {"First", 150.0},
+        {"Business", 80.0},
+        {"Economy", 40.0}
+    }
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 1. Set up your dropdown selection items
+        cmbCabinClass.Items.Add("First Class ($150)")
+        cmbCabinClass.Items.Add("Business Class ($80)")
+        cmbCabinClass.Items.Add("Economy Class ($40)")
+
+        lblPriceTotal.Text = "Seat Price Total: $0.00"
+
+        ' 2. Initialize our hardcoded layout data into the array
+        SetupHardcodedSeats()
+
+        ' 3. Render the full plane layout immediately so it's always showing
+        RenderEntirePlaneGrid()
+    End Sub
+
+    ''' <summary>
+    ''' Hardcodes all 60 seats with their specific categories and initial booking status
+    ''' </summary>
+    Private Sub SetupHardcodedSeats()
+        Dim index As Integer = 0
+
+        For r As Integer = 0 To 9 ' 10 Rows (0 to 9)
+            For c As Integer = 0 To 5 ' 6 Columns (A to F)
+                Dim letter As String = Chr(65 + c) ' Converts 0->A, 1->B, etc.
+                Dim seatNo As String = (r + 1).ToString() & letter
+
+                seatArray(index).SeatNumber = seatNo
+
+                ' Hardcoding categories by rows
+                If r < 2 Then
+                    seatArray(index).CabinClass = "First"   ' Rows 1-2
+                ElseIf r < 5 Then
+                    seatArray(index).CabinClass = "Business" ' Rows 3-5
+                Else
+                    seatArray(index).CabinClass = "Economy"  ' Rows 6-10
+                End If
+
+                ' Hardcoding some random pre-booked seats to show the grey color working
+                If seatNo = "1A" Or seatNo = "4C" Or seatNo = "8E" Then
+                    seatArray(index).IsBooked = True
+                Else
+                    seatArray(index).IsBooked = False
+                End If
+
+                index += 1
+            Next
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Draws the complete plane structure inside TableLayoutPanel1 and TableLayoutPanel2
+    ''' </summary>
+    Private Sub RenderEntirePlaneGrid()
+        TableLayoutPanel1.Controls.Clear()
+        TableLayoutPanel2.Controls.Clear()
+
+        Dim rows As Integer = 10
+        Dim colsPerPanel As Integer = 3
+
+        For r As Integer = 0 To rows - 1
+
+            ' --- LEFT SEATING BLOCK (Columns A, B, C) ---
+            For c As Integer = 0 To colsPerPanel - 1
+                Dim seatLetter As String = Chr(65 + c)
+                Dim targetSeatName As String = (r + 1).ToString() & seatLetter
+
+                Dim seatObj As SeatData = FindSeatInArray(targetSeatName)
+                Dim btn As Button = BuildSeatButton(seatObj)
+                TableLayoutPanel1.Controls.Add(btn, c, r)
+            Next
+
+            ' --- RIGHT SEATING BLOCK (Columns D, E, F) ---
+            For c As Integer = 0 To colsPerPanel - 1
+                Dim seatLetter As String = Chr(68 + c)
+                Dim targetSeatName As String = (r + 1).ToString() & seatLetter
+
+                Dim seatObj As SeatData = FindSeatInArray(targetSeatName)
+                Dim btn As Button = BuildSeatButton(seatObj)
+                TableLayoutPanel2.Controls.Add(btn, c, r)
+            Next
+        Next
+    End Sub
+
+    Private Function BuildSeatButton(seat As SeatData) As Button
+        Dim btnSeat As New Button()
+        btnSeat.Dock = DockStyle.Fill
+        btnSeat.Text = seat.SeatNumber
+        btnSeat.FlatStyle = FlatStyle.Flat
+        btnSeat.FlatAppearance.BorderSize = 1
+        btnSeat.Tag = seat ' Attaches our structural data right to the button control
+
+        ' If it's pre-booked, turn it grey immediately
+        If seat.IsBooked Then
+            btnSeat.BackColor = Color.DarkGray
+            btnSeat.Enabled = False
+        Else
+            btnSeat.BackColor = Color.White
+            AddHandler btnSeat.Click, AddressOf Seat_Click
+        End If
+
+        Return btnSeat
+    End Function
+
+    ''' <summary>
+    ''' Handles clicking a seat. Validates if it matches the chosen dropdown cabin class.
+    ''' </summary>
+    Private Sub Seat_Click(sender As Object, e As EventArgs)
+        ' Check if a class has been selected first
+        If cmbCabinClass.SelectedIndex = -1 Then
+            MessageBox.Show("Please select your desired Cabin Class first!", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Exit Sub
+        End If
+
+        Dim btn As Button = CType(sender, Button)
+        Dim seat As SeatData = CType(btn.Tag, SeatData)
+
+        ' Translate dropdown text into standard keys ("First", "Business", "Economy")
+        Dim currentSelectedClass As String = ""
+        If cmbCabinClass.SelectedItem.ToString().Contains("First Class") Then currentSelectedClass = "First"
+        If cmbCabinClass.SelectedItem.ToString().Contains("Business Class") Then currentSelectedClass = "Business"
+        If cmbCabinClass.SelectedItem.ToString().Contains("Economy Class") Then currentSelectedClass = "Economy"
+
+        ' Mismatch validation rule
+        If seat.CabinClass <> currentSelectedClass Then
+            MessageBox.Show($"This seat is a {seat.CabinClass} Class seat. Please select a seat inside the {currentSelectedClass} section.", "Class Mismatch", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Dim standardCost As Decimal = seatPriceMap(seat.CabinClass)
+
+        ' Toggle selection color (White <--> Lime Green)
+        If btn.BackColor = Color.White Then
+            btn.BackColor = Color.Lime
+            selectedButtons.Add(btn)
+            totalPrice += standardCost
+        ElseIf btn.BackColor = Color.Lime Then
+            btn.BackColor = Color.White
+            selectedButtons.Remove(btn)
+            totalPrice -= standardCost
+        End If
+
+        lblPriceTotal.Text = "Seat Price Total: $" & totalPrice.ToString("N2")
+    End Sub
+
+    ''' <summary>
+    ''' Fires when the Continue button is hit. Turns green selections to Grey permanently.
+    ''' </summary>
+    Private Sub btnContinue_Click(sender As Object, e As EventArgs) Handles btnContinue.Click
+        If selectedButtons.Count = 0 Then
+            MessageBox.Show("Please select at least one seat before continuing.", "No Seats Selected", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        ' Loop through current selections and lock them to Grey (Not Available)
+        For Each btn As Button In selectedButtons
+            btn.BackColor = Color.DarkGray
+            btn.Enabled = False
+
+            ' Update the local array status so it remembers the state change
+            For i As Integer = 0 To seatArray.Length - 1
+                If seatArray(i).SeatNumber = btn.Text Then
+                    seatArray(i).IsBooked = True
+                    Exit For
+                End If
+            Next
+        Next
+
+        MessageBox.Show($"Seats confirmed! Total Charge: ${totalPrice.ToString("N2")}", "Booking Saved", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        ' Reset selection counters for the next customer purchase simulation
+        selectedButtons.Clear()
+        totalPrice = 0
+        lblPriceTotal.Text = "Seat Price Total: $0.00"
+    End Sub
+
+    ' Helper to query our structural array items by seat identification names
+    Private Function FindSeatInArray(seatNo As String) As SeatData
+        For Each seat As SeatData In seatArray
+            If seat.SeatNumber = seatNo Then Return seat
+        Next
+        Return New SeatData()
+    End Function
+End Class
